20ae3e012408ae031df804048b47d3a712b64a14
bakthasingh-techsophy <bakthasingh.e@techsophy.com>
2025-01-07 11:46:49 +0530

    layout changes

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,3 +1,4 @@
 export { default as defaultLogo } from "./react.svg";
 export { default as backgroundPic1 } from "./background1.jpg";
 export { default as cardPic1 } from "./card1.jpg";
+export { default as backgroundPic2 } from "./background2.jpg";
